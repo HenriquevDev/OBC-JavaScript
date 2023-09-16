2ea8f3f70e18195d6ecf0526b8cefec4ac3b9c1e
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-09-16 18:40:47 -0300

    escopo

---
+++ aulas/014Escopo/escopo.js
@@ -0,0 +1,54 @@
+console.log(nome)
+// console.log(sobrenome)
+
+var nome = "Henrique"
+let sobrenome = "Vieira"
+
+console.log(nome)
+// console.log(sobrenome)
+
+
+
+// let pokemon = "Charmander"
+
+// function evoluir(){
+//     pokemon = "Charmeleon"
+// }
+
+// console.log(pokemon)
+// evoluir()
+// console.log(pokemon)
+
+// // . . . . . .
+
+// function criarAnimal(){
+//     let animal = "gato"
+// }
+
+// criarAnimal()
+// // console.log(animal)
+
+// // . . . . . .
+
+// function avaliarNota(nota){
+//     if (nota > 60){
+//         var aprovado = true
+//         let situacao = "Aprovado"
+//     } else {
+//         var aprovado = false
+//         let situacao = "Reprovado"
+//     }
+
+//     console.log(nota)
+//     console.log(aprovado)
+//     console.log(situacao)
+// }
+
+// // avaliarNota(65)
+// // avaliarNota(23)
+
+// function ola(){
+//     var texto = "Ola mundo"
+// }
+
+// console.log(texto)
